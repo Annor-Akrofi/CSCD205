a30514ef9a5da5455c909e1e9eea76707a322a4f
Annor-Akrofi <31506140+Annor-Akrofi@users.noreply.github.com>
2018-08-30 08:34:48 +0100

    Add files via upload

---
+++ a2_10682567.cc.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+   int number;
+   int a;
+   int b = 0;
+
+   cout << "Enter  the number you want to determine: "<<endl;
+   cin >> number;
+
+
+   for (a = 1; a <= number; a++)
+   {
+      if ((number % a) == 0)
+      {
+         b++;
+      }
+   }
+
+   if (b == 2)
+      cout << number << " is a prime number";
+   else
+      cout <<  " enter a new number. This number is not prime";
+
+   return 0;
+}
+
